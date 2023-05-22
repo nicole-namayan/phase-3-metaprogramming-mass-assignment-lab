357f552aea79e8011d5bbec5e3a1dd3f9b408d55
nicole <nicolenamayan133@gmail.com>
2023-05-22 22:50:16 +0300

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
-  # your code here
+  ATTRIBUTES = [:name, :birthday, :hair_color, :eye_color, :height,
+                :weight, :handed, :complexion, :t_shirt_size,
+                :wrist_size, :glove_size, :pant_length, :pant_width]
+
+  attr_accessor *ATTRIBUTES
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      if ATTRIBUTES.include?(key.to_sym)
+        self.send("#{key}=", value)
+      end
+    end
+  end
 end
